575b1e54070c7e479fe4d5a614f653c382417076
grayniks <nikola426@mail.ru>
2024-12-13 17:40:05 +0300

    Добавил сортировку слиянием с использованием буфера

--- Sort/merge_sort.py
+++ Sort/merge_sort.py
@@ -35,7 +35,58 @@ def sort_by_AI(arr):
             k += 1
 
 
+"""Сортировка слиянием с использованием буфера"""
+
+def buf_sort(arr, left, right, buf):
+    if (right - left) <= 1:
+        return
+
+    mid = (left + right) // 2
+
+    buf_sort(arr, left, mid, buf)
+    buf_sort(arr, mid, right, buf)
+    merge(arr, left, mid, right, buf)
+
+def merge(arr, l, m, r, buf):
+    p1, p2 = l, m
+    rp = 0
+
+    while (p1 < m) or (p2 < r):
+        if p1 == m:
+            buf[rp] = arr[p2]
+            rp += 1
+            p2 += 1
+            continue
+        if p2 == r:
+            buf[rp] = arr[p1]
+            rp += 1
+            p1 += 1
+            continue
+        if arr[p1] < arr[p2]:
+            buf[rp] = arr[p1]
+            rp += 1
+            p1 += 1
+        else:
+            buf[rp] = arr[p2]
+            rp += 1
+            p2 += 1
+
+    for i in range(l, r):
+        arr[i] = buf[i - l]
+
+def sort(arr):
+    len_arr = len(arr)
+    buf = [None] * len_arr
+
+    buf_sort(arr, 0, len_arr, buf)
+
+
 nums_list = [-4, 0, 23, -9, -4, 77, 0, 33, -9]
 
 sort_by_AI(nums_list)
+print(nums_list)
+
+nums_list = [-4, 0, 23, -9, -4, 77, 0, 33, -9]
+
+sort(nums_list)
 print(nums_list)
