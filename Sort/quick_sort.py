d5dc13c6467ec99c83156c4857cd5e1a1b91caca
grayniks <nikola426@mail.ru>
2024-12-15 14:42:38 +0300

    Заменил быструю сортировку на оптимизированную со случайным выбором опорного элемента и меньшей глубиной рекурсии

--- Sort/quick_sort.py
+++ Sort/quick_sort.py
@@ -1,11 +1,28 @@
+"""
+Оптимизировання быстрая сортировка со случайным выбором опорного элемента и меньшей глубиной рекурсии.
+Больше всего подходит для больших массивов.
+"""
+
+from random import randint
+
+
 def quicksort(arr, left, right):
-    if left < right:
+    while left < right:
+        pivot_index = randint(left, right)  # Случайный выбор опорного элемента
+        arr[pivot_index], arr[right] = arr[right], arr[pivot_index]  # Перемещаем его в конец
         pivot_index = partition(arr, left, right)
-        quicksort(arr, left, pivot_index - 1)  # Рекурсивно сортируем левую часть
-        quicksort(arr, pivot_index + 1, right)  # Рекурсивно сортируем правую часть
+
+        # Оптимизация: сортируем меньшую часть рекурсивно
+        if pivot_index - left < right - pivot_index:
+            quicksort(arr, left, pivot_index - 1)  # Сортируем левую часть
+            left = pivot_index + 1  # Сортируем правую часть в цикле
+        else:
+            quicksort(arr, pivot_index + 1, right)  # Сортируем правую часть
+            right = pivot_index - 1  # Сортируем левую часть в цикле
+
 
 def partition(arr, left, right):
-    pivot = arr[right]  # Выбираем последний элемент в качестве опорного
+    pivot = arr[right] # Выбираем случайный элемент в качестве опорного
     i = left - 1  # Индекс меньшего элемента
 
     for j in range(left, right):
@@ -16,6 +33,7 @@ def partition(arr, left, right):
     # Перемещаем опорный элемент на правильную позицию
     arr[i + 1], arr[right] = arr[right], arr[i + 1]
     return i + 1  # Возвращаем индекс опорного элемента
+
 
 # Пример использования
 nums = [-4, 0, 23, -9, -4, 77, 0, 33, -9]
